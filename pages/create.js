a3285e0d324f82fdc42d360a38eddb54cad082ab
Dennis Wang <denniswang812@gmail.com>
2022-01-17 09:10:45 -0800

    tags are SO meta

--- pages/create.js
+++ pages/create.js
@@ -1,10 +1,20 @@
 import { Button, Center, Text, VStack } from "@chakra-ui/react";
+import Head from "next/head";
 import Link from "next/link";
 import LettuceForm from "../lib/LettuceForm";
 
 export default function Page({}) {
   return (
     <>
+<Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Lettis - create" />
+        <meta name="author" content="Dennis Wang" />
+        <meta property="og:site_name" content="Lettis" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="../public/lettis.jpg" />
+        <title>Lettis - Create</title>
+      </Head>
       <VStack>
         <Text fontSize={50}>Create your lettuce!</Text>
         <LettuceForm></LettuceForm>
--- pages/lookinforlettuce.js
+++ pages/lookinforlettuce.js
@@ -1,11 +1,20 @@
 import { Button } from "@chakra-ui/button";
 import { Box, Center, Link, Text, VStack } from "@chakra-ui/layout";
+import Head from "next/head";
 import SearchForm from "../lib/SearchForm";
 
 export default function Page({}) {
   return (
     <>
-
+<Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Lettis - search" />
+        <meta name="author" content="Dennis Wang" />
+        <meta property="og:site_name" content="Lettis" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="../public/lettis.jpg" />
+        <title>Lettis - Search</title>
+      </Head>
       <Link href="/">
         <Button pos={"absolute"} left={3} top={3}>
           Back Home{" "}
